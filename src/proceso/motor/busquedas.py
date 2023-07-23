0f804ac6eabce55786f0f11f60f2d4874b0086d9
lliwi <31549684+lliwi@users.noreply.github.com>
2023-07-23 18:56:28 +0200

    Update busquedas.py

--- src/proceso/motor/busquedas.py
+++ src/proceso/motor/busquedas.py
@@ -2,11 +2,12 @@ from colorama import Fore, init
 init(autoreset=True)
 from proceso.motor.scrapeo import *
 def enviar (n):
+    n = n.replace("+", "")
     n = n.replace(" ", "")
     n = n.replace("(", "")
     n = n.replace(")", "")
     n = n.replace("-", "")
-    if(len(n) < 12):
+    if(len(n) < 7):
         print(Fore.RED+"Le faltan numeros a este telefono.")
         return
     pais = n[0:3]
